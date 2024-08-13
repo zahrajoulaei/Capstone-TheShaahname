0c247c1758b50afb8dee3fdfb09807aec40dc341
ZahraJou <zahrajoulaei@gmail.com>
2024-08-13 18:11:26 -0400

    db config file complete

--- Backend/configs/db.config.js
+++ Backend/configs/db.config.js
@@ -1,11 +1,15 @@
-const mongoose = require('mongoose');
+//Imports the Mongoose library
+const mongoose = require("mongoose");
 
+//Function to establish a connection to the MongoDB database
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.ATLAS_URI);
     console.log("Connected to MongoDB");
   } catch (err) {
     console.error("Failed to connect to MongoDB", err);
+
+    // Exit the process with a failure code (1) if unable to connect to the database
     process.exit(1);
   }
 };
